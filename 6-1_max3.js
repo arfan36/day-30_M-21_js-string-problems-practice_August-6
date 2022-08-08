c726f6dbd3879df23170383924a633b03a424847
arfan36 <arfanur36@gmail.com>
2022-08-08 11:41:02 +0600

    21-6 Can you find out who

---
+++ 6-1_max3.js
@@ -0,0 +1,22 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jime > chinku) {
+    console.log('Jim will get the cake');
+}
+else if (dela > jim && dela > chinku) {
+    console.log('Dela will eat cake in this bala');
+}
+else {
+    console.log('Chinku will get the cake');
+}
+
+console.log(Math.max(12, 45));
+
+// Homework: 1
+// write a functon that will take 3 numbers will return the max number
+// Write a functon that will taka 3 parameters and will return the min number
+
+// first time do it using if-else
+// second time do it using Math.min or Math.max
